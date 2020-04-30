2ba2b49153f31917646bfd8934cb01e0e8bf93cc
unrealguthrie <unrealguthrie@gmail.com>
2020-04-30 04:07:26 +0200

    regup

--- example1/udp_client.c
+++ example1/udp_client.c
@@ -1,78 +1,95 @@
-// UDP hole punching example, client code
-// Base UDP code stolen from http://www.abc.se/~m6695/udp.html
-// By Oscar Rodriguez
-// This code is public domain, but you're a complete lunatic
-// if you plan to use this code in any real program.
- 
+/*
+ * UDP hole punching example, client code
+ * Base UDP code stolen from http://www.abc.se/~m6695/udp.html
+ * By Oscar Rodriguez
+ * This code is public domain, but you're a complete lunatic
+ * if you plan to use this code in any real program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
- 
+
 #define BUFLEN 512
 #define NPACK 10
-#define PORT 9930
- 
-// This is our server's IP address. In case you're wondering, this one is an RFC 5737 address.
+
+/*
+ * This is our server's IPv4-address and port.
+ */
 #define SRV_IP "78.46.187.177"
-//#define SRV_IP "127.0.0.1"
- 
-// A small struct to hold a UDP endpoint. We'll use this to hold each peer's endpoint.
+#define SRV_PORT 9930
+
+/*
+ * A small struct to hold a UDP endpoint. We'll use this to hold each 
+ * peer's endpoint.
+ */
 struct peer
 {
-    unsigned int host;
-    unsigned int port;
+	unsigned int addr;
+	unsigned short port;
 };
- 
-// Just a function to kill the program when something goes wrong.
-void diep(char *s)
+
+int main(int argc, char **argv)
 {
-    perror(s);
-    exit(1);
-}
- 
-int main(int argc, char* argv[])
-{
-    struct sockaddr_in si_other;
-    int s, i, f, j, k, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    struct peer other;
- 
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-        diep("socket");
- 
-    // The server's endpoint data
-    memset((char *) &si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
-    if (inet_aton(SRV_IP, &si_other.sin_addr)==0)
-        diep("aton");
- 
-    if (sendto(s, "hi", 2, 0, (struct sockaddr*)(&si_other), slen)==-1)
-        diep("sendto");
- 
-    if (recvfrom(s, &other, sizeof(other), 0, (struct sockaddr*)(&si_other), &slen)==-1)
-        diep("recvfrom");
-    printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+	struct sockaddr_in si_other;
+	unsigned int s_sz = sizeof(struct sockaddr_in);
+	int sock;
+	int i;
+	char buf[BUFLEN];
+	struct peer other;
+	unsigned int p_sz = sizeof(struct peer);
+	struct sockaddr *si_ptr = (struct sockaddr *)&si_other;
 
-    si_other.sin_addr.s_addr = htonl(other.host);
-    si_other.sin_port = htons(other.port);
-    printf("add peer %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		perror("socket()");
+		return -1;
+	}
 
-    for (k = 0; k < 10; k++)
-    {
-        if (sendto(s, "hi", 2, 0, (struct sockaddr*)(&si_other), slen)==-1)
-            diep("sendto()");
-        if (recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*)(&si_other), &slen)==-1)
-            diep("recvfrom()");
-        printf("Received packet %s from %s:%d\n", buf, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-    }
+	memset(&si_other, 0, s_sz);
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(SRV_PORT);
+	if(inet_aton(SRV_IP, &si_other.sin_addr) < 0) {
+		perror("aton()");
+		goto err_close_sock;
+	}
 
-    // Actually, we never reach this point...
-    close(s);
-    return 0;
+	if(sendto(sock, "hi", 2, 0, si_ptr, s_sz) < 0) {
+		perror("sendto()");
+		goto err_close_sock;
+	}
+
+	if(recvfrom(sock, &other, p_sz, 0, si_ptr, &s_sz) < 0) {
+		perror("recv()");
+		goto err_close_sock;
+	}
+
+	si_other.sin_addr.s_addr = htonl(other.addr);
+	si_other.sin_port = htons(other.port);
+	printf("add peer %s:%d\n", inet_ntoa(si_other.sin_addr),
+			ntohs(si_other.sin_port));
+
+	for(i = 0; i < 10; i++) {
+		if(sendto(sock, "hi", 2, 0, si_ptr, s_sz) < 0) {
+			perror("sendto()");
+			goto err_close_sock;
+		}
+
+		if(recvfrom(sock, buf, BUFLEN, 0, si_ptr, &s_sz) < 0) {
+			perror("recvfrom()");
+			goto err_close_sock;
+		}
+
+		printf("Received packet %s from %s:%d\n", buf, 
+				inet_ntoa(si_other.sin_addr),
+				ntohs(si_other.sin_port));
+	}
+
+err_close_sock:
+	close(sock);
+	return 0;
 }
--- example1/udp_server.c
+++ example1/udp_server.c
@@ -1,72 +1,86 @@
-// UDP hole punching example, server code
-// Base UDP code stolen from http://www.abc.se/~m6695/udp.html
-// By Oscar Rodriguez
-// This code is public domain, but you're a complete lunatic
-// if you plan to use this code in any real program.
- 
+/*
+ * UDP hole punching example, server code
+ * Base UDP code stolen from http://www.abc.se/~m6695/udp.html
+ * By Oscar Rodriguez
+ * This code is public domain, but you're a complete lunatic
+ * if you plan to use this code in any real program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
- 
+
 #define BUFLEN 512
 #define PORT 9930
- 
-// A small struct to hold a UDP endpoint. We'll use this to hold each client's endpoint.
+
+/* 
+ * A small struct to hold a UDP endpoint. We'll use this to hold each client's
+ * endpoint.
+ */
 struct peer {
-    unsigned int host;
-    unsigned int port;
+	unsigned int addr;
+	unsigned short port;
 };
- 
-// Just a function to kill the program when something goes wrong.
-void diep(char *s)
-{
-    perror(s);
-    exit(1);
-}
- 
-int main(void)
-{
-    struct sockaddr_in si_me, si_other;
-    int s, i, j, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    struct peer peers[2];
-    int n = 0;
- 
-    // Create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-        diep("socket");
- 
-    // server cannot be behind a NAT.
-    memset((char *) &si_me, 0, sizeof(si_me));
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(s, (struct sockaddr*)(&si_me), sizeof(si_me))==-1)
-        diep("bind");
- 
-    for (i = 0; i < 2; i++) {
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr*)(&si_other), &slen)==-1)
-            diep("recvfrom");
-        peers[i].host = ntohl(si_other.sin_addr.s_addr);
-        peers[i].port = ntohs(si_other.sin_port);
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-    }
 
-    for (i = 0; i < 2; i++)
-    {
-        si_other.sin_addr.s_addr = htonl(peers[i].host);
-        si_other.sin_port = htons(peers[i].port);
+int main(int argc, char **argv)
+{		
+	struct sockaddr_in si_me;
+	struct sockaddr *s_me = (struct sockaddr  *)&si_me;
+	struct sockaddr_in si_other;
+	struct sockaddr *s_other = (struct sockaddr *)&si_other;
+	unsigned int s_sz = sizeof(si_other);
+	int sock, i;
+	char buf[BUFLEN];
+	struct peer peers[2];
+	unsigned int p_sz = sizeof(struct peer);
 
-        printf("Sending to %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        if (sendto(s, &peers[i ^ 1], sizeof(struct peer), 0, (struct sockaddr*)(&si_other), slen)==-1)
-            diep("sendto");
-    }
-    sleep(0);
-    close(s);
-    return 0;
+	if(argc || argv) {/* Prevent warning for not using parameters */}
+
+	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		perror("socket()");
+		return -1;
+	}
+
+	memset(&si_me, 0, s_sz);
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(PORT);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+	if(bind(sock, s_me, s_sz) < 0) {
+		perror("bind()");
+		goto err_close_sock;
+	}
+
+	for (i = 0; i < 2; i++) {
+		if(recvfrom(sock, buf, BUFLEN, 0, s_other, &s_sz) < 0) {
+			perror("recvfrom()");
+			goto err_close_sock;
+		}
+		peers[i].addr = ntohl(si_other.sin_addr.s_addr);
+		peers[i].port = ntohs(si_other.sin_port);
+		printf("Received packet from %s:%d\n", 
+				inet_ntoa(si_other.sin_addr),
+				ntohs(si_other.sin_port));
+	}
+
+	for (i = 0; i < 2; i++) {
+		si_other.sin_addr.s_addr = htonl(peers[i].addr);
+		si_other.sin_port = htons(peers[i].port);
+
+		printf("Sending to %s:%d\n", inet_ntoa(si_other.sin_addr),
+				ntohs(si_other.sin_port));
+
+		if(sendto(sock, &peers[i ^ 1], p_sz, 0, s_other, s_sz) < 0) {
+			perror("sendto()");
+			goto err_close_sock;
+		}
+	}
+
+err_close_sock:
+	close(sock);
+	return 0;
 }
