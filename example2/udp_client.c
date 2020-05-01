57d20ed7c9fbb09149ba425b6c9a8d0b360d2e1a
unrealguthrie <unrealguthrie@gmail.com>
2020-05-01 12:57:25 +0200

    added example2

---
+++ example2/udp_client.c
@@ -0,0 +1,151 @@
+/*
+ * UDP hole punching example, client code
+ * Base UDP code stolen from http://www.abc.se/~m6695/udp.html
+ * By Oscar Rodriguez
+ * This code is public domain, but you're a complete lunatic
+ * if you plan to use this code in any real program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define BUFLEN 512
+#define NPACK 10
+
+/*
+ * This is our server's IPv4-address and port.
+ */
+#define SRV_IP "78.46.187.177"
+#define SRV_PORT 9930
+
+#define SOCK_PORT 9931
+#define SOCK_NUM 5
+
+/*
+ * A small struct to hold a UDP endpoint. We'll use this to hold each 
+ * peer's endpoint.
+ */
+struct peer {
+	unsigned int addr;
+	unsigned short port;
+};
+
+struct sock {
+	int fd;
+	unsigned short port;
+	uint8_t used;
+};
+
+int main(int argc, char **argv)
+{
+	struct sockaddr_in si_other;
+	struct sockaddr_in si_recv;
+	struct sockaddr_in cli;
+	unsigned int s_sz = sizeof(struct sockaddr_in);
+	struct sock socks[SOCK_NUM];
+	int p, i;
+	char buf[BUFLEN];
+	struct peer other;
+	unsigned int p_sz = sizeof(struct peer);
+	struct sockaddr *si_ptr = (struct sockaddr *)&si_other;
+	struct timeval tv;
+	fd_set rfds;
+
+	tv.tv_sec = 0;
+	tv.tv_usec = 20000;
+
+	for(i = 0; i < SOCK_NUM; i++) {
+		soccks[i].fd = -1;
+	}
+
+	/*
+	 * Initialize all sockets and set them on different ports.
+	 */
+	for(i = 0; i < SOCK_NUM; i++) {	
+		if((socks[i].fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+			perror("socket()");
+			return -1;
+		}
+
+		socks[i].port = SOCK_PORT + i;
+		socks[i].used = 0;
+
+		/*
+		 * Bind the socket to the designated port-number.
+		 */
+		memset(&cli, 0, s_sz);
+		cli.sin_family = AF_INET;
+		cli.sin_port = htons(socks[i].port);
+		cli.sin_addr.s_addr = htonl(INADDR_ANY);
+		if(bind(socks[i].fd, (struct sockaddr *)&cli, s_sz) < 0) {
+			perror("bind()");
+			goto err_close_sock;
+		}
+
+		/*
+		 * Set port non-blocking, if it's not the srver-port.
+		 */
+		if(i != 0) {
+			if(fcntl(socks[i].fd, F_SETFL, O_NONBLOCK) < 0) {
+				goto err_close_sock;	
+			}
+		}
+	}
+
+	memset(&si_other, 0, s_sz);
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(SRV_PORT);
+	if(inet_aton(SRV_IP, &si_other.sin_addr) < 0) {
+		perror("aton()");
+		goto err_close_sock;
+	}
+
+	if(sendto(socks[i].fd, &sock[i].port, sizeof(unsigned short), 0, si_ptr, s_sz) < 0) {
+		perror("sendto()");
+		goto err_close_sock;
+	}
+
+	if(recvfrom(socks[i].fd, &other, p_sz, 0, si_ptr, &s_sz) < 0) {
+		perror("recv()");
+		goto err_close_sock;
+	}
+
+	si_other.sin_addr.s_addr = htonl(other.addr);
+	si_other.sin_port = htons(other.port);
+	printf("add peer %s:%d\n", inet_ntoa(si_other.sin_addr),
+		ntohs(si_other.sin_port));
+
+	for(p = 0; p < 10; p++) {
+		if(sendto(sock, "hi\0", 3, 0, si_ptr, s_sz) < 0) {
+			perror("sendto()");
+			goto err_close_sock;
+		}
+
+		for(i = 0; i < SOCK_NUM; i++) {
+			FD_ZERO(&rfds);
+			FD_SET(socks[i].fd, &rfds);
+
+			if(select(socks[i].fd, &rfds, NULL, NULL, &tv) > 0) {
+				if(recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr *)&si_recv, &s_sz) > 0) {
+					printf("Received packet %s from %s:%d\n", buf, 
+						inet_ntoa(si_recv.sin_addr),
+						ntohs(si_recv.sin_port));
+				}
+			}
+		}
+	}
+
+err_close_sock:
+	for(i = 0; i < SOCK_NUM; i++) {
+		if(socks[i].fd > 0)
+			close(socks[i].fd);
+	}
+	return 0;
+}
---
+++ example2/udp_server.c
@@ -0,0 +1,89 @@
+/*
+ * UDP hole punching example, server code
+ * Base UDP code stolen from http://www.abc.se/~m6695/udp.html
+ * By Oscar Rodriguez
+ * This code is public domain, but you're a complete lunatic
+ * if you plan to use this code in any real program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFLEN 512
+#define PORT 9930
+
+/* 
+ * A small struct to hold a UDP endpoint. We'll use this to hold each client's
+ * endpoint.
+ */
+struct peer {
+	unsigned int addr;
+	unsigned short port;
+};
+
+int main(int argc, char **argv)
+{		
+	struct sockaddr_in si_me;
+	struct sockaddr *s_me = (struct sockaddr  *)&si_me;
+	struct sockaddr_in si_other;
+	struct sockaddr *s_other = (struct sockaddr *)&si_other;
+	unsigned int s_sz = sizeof(si_other);
+	int sock, i;
+	char buf[BUFLEN];
+	struct peer peers[2];
+	unsigned int p_sz = sizeof(struct peer);
+
+	if(argc || argv) {/* Prevent warning for not using parameters */}
+
+	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		perror("socket()");
+		return -1;
+	}
+
+	memset(&si_me, 0, s_sz);
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(PORT);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+	if(bind(sock, s_me, s_sz) < 0) {
+		perror("bind()");
+		goto err_close_sock;
+	}
+
+	for (i = 0; i < 2; i++) {
+		if(recvfrom(sock, buf, BUFLEN, 0, s_other, &s_sz) < 0) {
+			perror("recvfrom()");
+			goto err_close_sock;
+		}
+		peers[i].addr = ntohl(si_other.sin_addr.s_addr);
+		peers[i].port = ntohs(*(unsigned short *)buf);
+		printf("Received packet from %s:%d\n", 
+				inet_ntoa(si_other.sin_addr),
+				ntohs(si_other.sin_port));
+	}
+
+	for (i = 0; i < 2; i++) {
+		si_other.sin_addr.s_addr = htonl(peers[i].addr);
+		si_other.sin_port = htons(peers[i].port);
+
+		printf("Sending to %s:%d\n", inet_ntoa(si_other.sin_addr),
+				ntohs(si_other.sin_port));
+
+		memcpy(buf, &peers[i ^ 1], p_sz);
+		*(buf + p_sz) = i;
+
+		if(sendto(sock, buf, p_sz + 1, 0, s_other, s_sz) < 0) {
+			perror("sendto()");
+			goto err_close_sock;
+		}
+	}
+
+err_close_sock:
+	close(sock);
+	return 0;
+}
